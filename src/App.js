7e4c52de8e49b7b61c626d2dd859951bdc5d37e8
Kaomi <maksim.burmistrov2006@gmail.com>
2026-04-16 19:57:00 +0300

    Список операций

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import "./App.css";
 
 function changeType() {
   let type = document.getElementById("type").value;
-  let tip = document.getElementById("tip");
+  let categ = document.getElementById("categ");
 
   if (type === "Доход") {
-    tip.innerHTML = "<option>Поступление</option>";
+    categ.innerHTML = "<option>Поступление</option>";
   } else {
-    tip.innerHTML = `
+    categ.innerHTML = `
       <option>Еда</option>
       <option>Электроника</option>
       <option>Одежда</option>
     `;
   }
+}
+
+function add() {
+  let type = document.getElementById("type").value;
+  let categ = document.getElementById("categ").value;
+  let sum = document.getElementById("sum").value;
+
+  let list = localStorage.getItem("list");
+  if (list == null) {
+    list = "";
+  }
+
+  list = list + type + " / " + categ + " / " + sum + "\n";
+  localStorage.setItem("list", list);
+
+  window.location.reload();
 }
 
 function App() {
@@ -30,7 +46,7 @@ function App() {
 
       <div className="lbl">
         <label>Категория: </label>
-        <select id="tip">
+        <select id="categ">
           <option>Поступление</option>
         </select>
       </div>
@@ -39,7 +55,15 @@ function App() {
         <label>Сумма: </label>
         <input id="sum" type="number" min="0"/>
       </div>
-      <button>Добавить</button>
+      <button onClick={add}>Добавить</button>
+
+      <div>
+        <h2>Список операций</h2>
+        <button onClick={()=>{localStorage.clear(); window.location.reload()}}>Очистить</button>
+        <pre>
+          {localStorage.getItem("list") == null ? "" : localStorage.getItem("list")}
+        </pre>
+      </div>
     </div>
   );
 }
